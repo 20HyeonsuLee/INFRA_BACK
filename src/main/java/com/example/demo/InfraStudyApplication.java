735abe65d92b88cf41a11879ec689be80de1847d
HyeonsuLee <leehyeonsu4888@naver.com>
2024-07-14 15:27:55 +0900

    테스트 api

--- src/main/java/com/example/demo/InfraStudyApplication.java
+++ src/main/java/com/example/demo/InfraStudyApplication.java
@@ -6,8 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class InfraStudyApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(InfraStudyApplication.class, args);
-	}
-
+    public static void main(String[] args) {
+        SpringApplication.run(InfraStudyApplication.class, args);
+    }
 }
---
+++ src/main/java/com/example/demo/config/SecurityConfig.java
@@ -0,0 +1,38 @@
+package com.example.demo;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+            .csrf().disable()
+            .authorizeRequests()
+            .anyRequest().permitAll()
+            .and()
+            .addFilter(corsFilter()); // CORS 필터 추가
+        return http.build();
+    }
+
+    @Bean
+    public CorsFilter corsFilter() {
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        CorsConfiguration config = new CorsConfiguration();
+        config.setAllowCredentials(true);
+        config.addAllowedOriginPattern("*"); // 모든 출처 허용
+        config.addAllowedHeader("*"); // 모든 헤더 허용
+        config.addAllowedMethod("*"); // 모든 HTTP 메서드 허용
+        source.registerCorsConfiguration("/**", config);
+        return new CorsFilter(source);
+    }
+}
