cd5b0c6724e524c24b23a0ede33b1d9e198034d1
HyeonsuLee <leehyeonsu4888@naver.com>
2024-12-22 15:15:20 +0900

    ignore 추가

--- src/main/java/com/example/demo/controller/Test.java
+++ src/main/java/com/example/demo/controller/Test.java
@@ -2,10 +2,19 @@ package com.example.demo.controller;
 
 import java.util.List;
 
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
 
+import com.example.demo.S3Service;
 import com.example.demo.entity.Member;
 import com.example.demo.repository.MemberRepository;
 
@@ -14,15 +23,42 @@ import com.example.demo.repository.MemberRepository;
 public class Test {
 
     private final MemberRepository memberRepository;
+    private final S3Service s3Service;
 
-    public Test(MemberRepository memberRepository) {
+    public Test(
+        MemberRepository memberRepository,
+        S3Service s3Service
+    ) {
         this.memberRepository = memberRepository;
+        this.s3Service = s3Service;
+    }
+
+    @PostMapping("/upload")
+    public ResponseEntity<String> uploadFile(
+        @RequestParam("file") MultipartFile file
+    ) {
+        try {
+            String fileUrl = s3Service.uploadFile(file);
+            return ResponseEntity.ok(fileUrl);
+        } catch (Exception e) {
+            return ResponseEntity.status(500).body("File upload failed: " + e.getMessage());
+        }
+    }
+
+    @GetMapping("/download/{key}")
+    public ResponseEntity<byte[]> downloadFile(@PathVariable String key) {
+        byte[] file = s3Service.downloadFile("images/" + key);
+
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.IMAGE_JPEG); // 이미지 타입에 맞게 MediaType을 설정
+        headers.setContentLength(file.length);
+
+        return new ResponseEntity<>(file, headers, HttpStatus.OK);
     }
 
     @GetMapping("/infra-study/test")
     public String test() {
         List<Member> members = memberRepository.findAll();
-        System.out.println("dsa: "+members.size());
         return members.stream().findFirst().get().getName();
     }
 }
