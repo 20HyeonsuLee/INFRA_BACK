27e52c1755e49afcc1006d78bf00b702e08aba88
HyeonsuLee <leehyeonsu4888@naver.com>
2025-01-23 21:48:23 +0900

    테스트

--- src/main/java/com/example/demo/repository/MemberRepository.java
+++ src/main/java/com/example/demo/repository/MemberRepository.java
@@ -5,9 +5,6 @@ import org.springframework.stereotype.Repository;
 
 import com.example.demo.entity.Member;
 
-
-
-
 @Repository
 public interface MemberRepository extends JpaRepository<Member, Integer> {
 
